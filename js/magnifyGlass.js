b6694698d7df98fd001d3f90ab6129cd56276cd9
Monsterduty <13luis13mc@gmail.com>
2023-02-08 04:57:50 -0500

    litle adjustment of the magnifying glass

--- js/magnifyGlass.js
+++ js/magnifyGlass.js
@@ -1,11 +1,17 @@
 function mouseHandler( event, frame, pos )
 {
-	let x = -pos.x + event.clientX
-	let y = -pos.y + event.clientY
-	let zoom = 8
+	let zoom = 5
+	let x = (((-pos.x + event.clientX) * zoom) - frame.offsetWidth / 2 )
+	let y = (((-pos.y + event.clientY) * zoom) - frame.offsetHeight / 2)
+
+	if ( x < 0 ) x = 0;
+	if ( x > (pos.width * zoom) - pos.width ) x = (pos.width * zoom) - pos.width
+	if ( y < 0 ) y = 0;
+	if ( y > (pos.height * zoom) - pos.height ) y = (pos.height * zoom) - pos.height
+
 	frame.style.backgroundSize = (pos.width * zoom) + "px " + (pos.height * zoom) + "px"
 	
-	frame.style.backgroundPosition = "-" + ((x * zoom) - frame.offsetWidth / 2 )+ "px -" + ((y * zoom) - frame.offsetHeight / 2) + "px"
+	frame.style.backgroundPosition = "-" + x + "px -" + y + "px"
 }
 
 function glass( item )
